997bc4c7b0300608190b288a25c41613771ed438
Jederson Andre <andre_sjx@live.com>
2024-05-14 16:15:11 -0300

    :sparkles: feat: implementação de arquivo que ira disponiblizar as fonts do sistema

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,12 @@
+
+import { Inter, Michroma } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const michroma = Michroma({
+    weight:'400',
+    subsets:['latin'],
+})
