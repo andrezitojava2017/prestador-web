ded0034b3ecb2d938066b657f4eea5bb7d5f22e3
Jederson Andre <andre_sjx@live.com>
2024-05-21 14:25:09 -0300

    :sparkles: feat: implementação da configuração do supabase; middleware que faz a verificação se o usuario está autenticando, verificando se há sessão ativa

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return NextResponse.rewrite(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+};
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
