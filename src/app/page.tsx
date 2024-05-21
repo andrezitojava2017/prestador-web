c33c5b90c2083a22688f2d79297dac41020bc144
Jederson Andre <andre_sjx@live.com>
2024-05-21 14:29:20 -0300

    :recycle: refactor: modificação na pagina principal da aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import { ChakraProvider, HStack, Stack } from "@chakra-ui/react";
+"use client";
+
+import { Flex, HStack } from "@chakra-ui/react";
 import BarraNavegacao from "@/componentes/navbar";
 import Head from "@/componentes/head/head";
+import HomePage from "./home/page";
 
-export default function Home({ children }: { children: React.ReactNode }) {
+export default function Home() {
   return (
-    <Stack height={'100vh'}>
-      <Head text="Pagina Principal"/>
-    </Stack>
+    <HStack height={"100vh"}>
+      <BarraNavegacao />
+      <Flex flexDirection={'column'} height={'100vh'} width={'100vw'}>
+        <Head text="Pagina Principal" buttonReturn={false} />
+        <HomePage />
+      </Flex>
+    </HStack>
   );
 }
