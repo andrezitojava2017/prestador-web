fb64ac4ab47d14146cb71b2efaad804f6ba9e361
Jederson Andre <andre_sjx@live.com>
2024-05-22 09:20:41 -0300

    :recycle: refactor: implementado logica para verificação e validação de dados

---
+++ src/componentes/modals/actions.ts
@@ -0,0 +1,70 @@
+"use client";
+
+import { Freelance } from "@/interface/freelance";
+import { consultaPisPasep } from "@/service/prestadorService";
+
+/**
+ * Remove números, pontos e caracteres especiais de uma string.
+ *
+ * @param {string} value Valor da string a ser processada.
+ * @returns {string} String com os caracteres indesejados removidos.
+ *
+ */
+export const verificarNomePrestador = (value: string): string => {
+  const regex = /[^a-zA-Z\s]/g;
+
+  const nomeVerificado = value.replace(regex, "");
+  return nomeVerificado;
+};
+
+/**
+ * Esta função formata um valor PIS/PASEP para armazenamento em banco de dados,
+ * removendo pontuação, letras do alfabeto e preservando espaços em branco.
+ * @param value Uma string contendo o valor PIS/PASEP a ser formatado
+ * @returns A função retorna uma string contendo o valor PIS/PASEP formatado, com apenas números e espaços em branco.
+ */
+export const formatarPisPasepParaBancoDeDados = (value: string) => {
+  // Expressão regular que remove pontuação e letras do alfabeto
+  const regex = /[^0-9\s]/g;
+  let pisPasep = value.replaceAll(regex, "");
+  return pisPasep;
+};
+
+/**
+ * verifica se o prestador já existe na base de dados
+ * @param freelance - dados do prestador para verificação
+ * @returns undefined - se não houver prestador
+ */
+export const verificarPrestadorExisteNaBase = async (freelance: Freelance) => {
+  const rs = await consultaPisPasep(freelance);
+
+  if (rs?.length !== 0) {
+    throw new Error(
+      `PIS-PASEP já existente na base de dados - Prestador: ${rs![0].nome}`
+    );
+  }
+  return;
+};
+
+/**
+ * Faz a verificação no objeto Freelance, set todas as informações
+ * foram fornecidas
+ * @param freelance parametro do tipo Freelance
+ */
+export const verificarCamposPreenchidos = (freelance: Freelance) => {
+  if (
+    freelance.nome === null ||
+    freelance.nome === "" ||
+    freelance.nome === undefined
+  ) {
+    throw new Error("Nome do prestador não foi informado");
+  }
+
+  if (
+    freelance.pisPasep === "" ||
+    freelance.pisPasep === null ||
+    freelance.pisPasep === undefined
+  ) {
+    throw new Error("PIS/PASEP do prestador não foi informado");
+  }
+};
