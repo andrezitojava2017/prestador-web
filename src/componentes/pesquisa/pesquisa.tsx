afce5747e45c451b59823e0da541c476040c6a1c
Jederson Andre <andre_sjx@live.com>
2024-05-22 17:21:04 -0300

    :sparkles: feat: implementação componente Pesquisa e sua logica para buscar dados de prestadores nabase

---
+++ src/componentes/pesquisa/pesquisa.tsx
@@ -0,0 +1,68 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import { Box, Button, Flex, HStack, Input, Text } from "@chakra-ui/react";
+
+type Pesquisa = {
+  setState: Dispatch<
+    SetStateAction<{ competencia: string; prestador: string }>
+  >;
+  dadosPesquisa: { competencia: string; prestador: string };
+  btnBuscar?: ReactNode;
+  ativaCompetencia: boolean;
+};
+
+const Pesquisa = (props: Pesquisa) => {
+  return (
+    <HStack>
+      <Flex
+        flexDirection={"row"}
+        alignItems={"baseline"}
+        gap={8}
+        border={"1px solid #106B87"}
+        borderRadius={8}
+        padding={4}
+      >
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          alignItems={"baseline"}
+          gap={2}
+        >
+          <Text>Competencia</Text>
+          <Input
+            isDisabled={!props.ativaCompetencia}
+            type="text"
+            width={"10vw"}
+            value={props.dadosPesquisa.competencia}
+            onChange={(text) =>
+              props.setState({
+                ...props.dadosPesquisa,
+                competencia: text.target.value,
+              })
+            }
+          />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          alignItems={"baseline"}
+          gap={2}
+        >
+          <Text>Prestador</Text>
+          <Input
+            type="text"
+            width={"28vw"}
+            value={props.dadosPesquisa.prestador}
+            onChange={(text) =>
+              props.setState({
+                ...props.dadosPesquisa,
+                prestador: text.target.value,
+              })
+            }
+          />
+        </Box>
+        <Box>{props.btnBuscar}</Box>
+      </Flex>
+    </HStack>
+  );
+};
+export default Pesquisa;
