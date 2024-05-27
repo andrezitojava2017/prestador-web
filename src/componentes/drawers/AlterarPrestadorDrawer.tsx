3b0e657984f49e30a22da012028188905f82f4fb
Jederson Andre <andre_sjx@live.com>
2024-05-27 17:10:26 -0300

    :recycle: refactor: implementação de logica para atualização de dados do prestador; implementado tratamento de erros se houver e mensagem toast

--- src/componentes/drawers/AlterarPrestadorDrawer.tsx
+++ src/componentes/drawers/AlterarPrestadorDrawer.tsx
@@ -12,11 +12,13 @@ import {
   useDisclosure,
   Input,
   Button,
+  useToast,
 } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import { FaUserEdit } from "react-icons/fa";
 import FormularioPrestador from "../FormularioPrestador/FormularioPrestador";
 import { FreelanceContexts } from "@/context/FreelanceContext";
+import { AtualizarDadosPrestador } from "./action";
 
 type Props = {
   data: Freelance;
@@ -24,11 +26,32 @@ type Props = {
 
 const AlterarPrestadorDrawer = (props: Props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [loading, setLoading] = useState<boolean>(false);
 
+  const toast = useToast();
 
-  const {setFreelancers, freelancers} = useContext(FreelanceContexts)
+  const { setFreelancers, freelancers } = useContext(FreelanceContexts);
 
-  
+  const atualizarInfo = async () => {
+    setLoading(true);
+
+    try {
+
+      await AtualizarDadosPrestador(freelancers);
+      setLoading(false);
+
+    } catch (error: any) {
+
+      toast({
+        title: "Atenção",
+        description: `${error.message}`,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
   useEffect(() => {
     if (props.data) setFreelancers(props.data);
   }, [props.data]);
@@ -39,15 +62,16 @@ const AlterarPrestadorDrawer = (props: Props) => {
         flexDirection={"column"}
         alignItems={"center"}
         onClick={() => onOpen()}
+        _hover={{cursor:'pointer'}}
       >
         <Text fontSize={10}>Editar</Text>
         <FaUserEdit size={18} color="green" />
       </Flex>
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} size={'md'}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
+          <DrawerHeader>Alterar Dados de Prestador</DrawerHeader>
 
           <DrawerBody>
             <FormularioPrestador />
@@ -55,10 +79,15 @@ const AlterarPrestadorDrawer = (props: Props) => {
 
           <DrawerFooter>
             <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
+              Cancelar
             </Button>
-            <Button colorScheme="blue" onClick={() => console.log(freelancers)}>
-              Save
+            <Button
+              colorScheme="blue"
+              isLoading={loading}
+              loadingText={'Aguarde...'}
+              onClick={() => atualizarInfo()}
+            >
+              Atualizar
             </Button>
           </DrawerFooter>
         </DrawerContent>
