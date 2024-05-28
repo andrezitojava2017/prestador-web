a58f80861b1d58dc2c82436f3d2e0a2ccdba650f
Jederson Andre <andre_sjx@live.com>
2024-05-28 15:02:43 -0300

    :sparkles: feat: implementado funcao para verificar se os campos foram preenchidos

--- src/componentes/drawers/action.ts
+++ src/componentes/drawers/action.ts
@@ -1,7 +1,31 @@
+import { ITributos } from "@/interface/ITributos";
 import { Freelance } from "@/interface/freelance";
 import { AtualizarDadosPrestadorService } from "@/service/prestadorService";
 
-export const AtualizarDadosPrestador =  async(data : Freelance)=>{
+export const AtualizarDadosPrestador = async (data: Freelance) => {
+  await AtualizarDadosPrestadorService(data);
+};
 
-    await AtualizarDadosPrestadorService(data)
-}
+export const validarDadosConfiguracaoTributo = (tributo: ITributos) => {
+  if (
+    tributo.competencia === null ||
+    tributo.competencia === undefined ||
+    tributo.competencia === ""
+  ) {
+    throw new Error("Competencia não foi informada");
+  }
+  if (
+    tributo.max_recolhimento === null ||
+    tributo.max_recolhimento === undefined ||
+    tributo.max_recolhimento === 0
+  ) {
+    throw new Error("Maximo de recolhimento não foi informada");
+  }
+  if (
+    tributo.patronal === null ||
+    tributo.patronal === undefined ||
+    tributo.patronal === 0
+  ) {
+    throw new Error("Patronal não foi informada");
+  }
+};
