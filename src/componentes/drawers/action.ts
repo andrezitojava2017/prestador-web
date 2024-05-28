25dc99d14fa334bb9834ef704d1288d2b688f0a5
Jederson Andre <andre_sjx@live.com>
2024-05-28 15:54:23 -0300

    :sparkles: feat: implementado logica para verificar campos do drawer adicionar secretaria

--- src/componentes/drawers/action.ts
+++ src/componentes/drawers/action.ts
@@ -1,3 +1,4 @@
+import { ISecretaria } from "@/interface/ISecretaria";
 import { ITributos } from "@/interface/ITributos";
 import { Freelance } from "@/interface/freelance";
 import { AtualizarDadosPrestadorService } from "@/service/prestadorService";
@@ -27,5 +28,22 @@ export const validarDadosConfiguracaoTributo = (tributo: ITributos) => {
     tributo.patronal === 0
   ) {
     throw new Error("Patronal não foi informada");
+  }
+};
+
+export const validarDadosSecretaria = (secretaria: ISecretaria) => {
+  if (
+    secretaria.codigo === null ||
+    secretaria.codigo === 0 ||
+    secretaria.codigo === undefined
+  ) {
+    throw new Error("Codigo nao foi informado");
+  }
+  if (
+    secretaria.descricao === null ||
+    secretaria.descricao === "" ||
+    secretaria.descricao === undefined
+  ) {
+    throw new Error("Descrição nao foi informado");
   }
 };
