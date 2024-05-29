9922b9d0c52146459921452e4eb5e7d3045ad6a0
Jederson Andre <andre_sjx@live.com>
2024-05-29 16:37:41 -0300

    :sparkles: feat: implementado interface ISecretaria

---
+++ src/interface/IServico.ts
@@ -0,0 +1,12 @@
+
+export interface IServico {
+    id?: number,
+    competencia:string,
+    empenho:number,
+    fonte:number,
+    pispasep?:number,
+    inss_retido:number,
+    inss_patronal:number,
+    salario_base:number,
+    cod_lotacao:number,
+}
