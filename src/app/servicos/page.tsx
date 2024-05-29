50f65d227649d27c533b4329cba2614dd43f532b
Jederson Andre <andre_sjx@live.com>
2024-05-29 16:48:38 -0300

    :recycle: refactor: inclusao de Link para a routa 'servicos/lancar'

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -1,28 +1,29 @@
-
 import Head from "@/componentes/head/head";
 import { Flex, HStack } from "@chakra-ui/react";
 import { VscNewFolder } from "react-icons/vsc";
 import { MdViewKanban } from "react-icons/md";
 import BarraNavegacao from "@/componentes/navbar";
 import ButtonService from "@/componentes/buttons/buttonService";
+import Link from "next/link";
 
 const Servicos = () => {
   return (
     <HStack height={"100vh"}>
       <BarraNavegacao />
-      <Flex flexDirection={'column'} height={'100vh'} >
-      <Head text="Serviços" buttonReturn={true} />
-      <HStack gap={8} marginLeft={6}>
-        <ButtonService
-          description="Novo Serviço"
-          icon={<VscNewFolder size={50} color="white" />}
-          
-        />
-        <ButtonService
-          description="Listar serviços"
-          icon={<MdViewKanban size={50} color="white" />}
-        />
-      </HStack>
+      <Flex flexDirection={"column"} height={"100vh"}>
+        <Head text="Serviços" buttonReturn={true} />
+        <HStack gap={8} marginLeft={6}>
+          <Link href={'/servicos/lancar'}>
+            <ButtonService
+              description="Novo Serviço"
+              icon={<VscNewFolder size={50} color="white" />}
+            />
+          </Link>
+          <ButtonService
+            description="Listar serviços"
+            icon={<MdViewKanban size={50} color="white" />}
+          />
+        </HStack>
       </Flex>
     </HStack>
   );
