b6af0381024e3c914348c7b6b5fc4317a294b917
Jederson Andre <andre_sjx@live.com>
2024-05-29 16:38:46 -0300

    :sparkles: feat: implementado função para recuperar lista de secretarias da base de dados

--- src/service/secretariaService.ts
+++ src/service/secretariaService.ts
@@ -7,9 +7,21 @@ export const adicionarNovaSecretaria = async (secretaria: ISecretaria) => {
     .insert(secretaria)
     .select();
 
+  if (error) {
+    console.warn("Ocorreu um erro na gravação\n", error.message);
+    throw new Error("Ocorreu um erro na gravação");
+  }
+  console.log(data);
+};
+
+export const recuperarListaSecretarias = async () => {
+  let { data: db_lotacao, error } = await supabase
+    .from("db_lotacao")
+    .select("*");
+
     if(error){
-        console.warn('Ocorreu um erro na gravação\n', error.message)
-        throw new Error('Ocorreu um erro na gravação')
+      console.warn('Ocorreu um erro na tentativa de recuperar lista de Lotação/Secretaria\n', error.message)
+      throw new Error('Ocorreu um erro na tentativa de recuperar lista de Lotação/Secretaria')
     }
-   console.log(data)
+    return db_lotacao
 };
