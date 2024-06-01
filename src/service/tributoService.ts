455cb1565221255819e5de4393f56ef2b6a81503
Jederson Andre <andre_sjx@live.com>
2024-05-31 22:26:45 -0300

    :sparkles: feat: implementado novo service para recuperar dados de tributos da base

--- src/service/tributoService.ts
+++ src/service/tributoService.ts
@@ -1,17 +1,32 @@
-import { ITributos } from "@/interface/ITributos";
-import supabase from "@/lib/supabase";
+import { ITributos } from '@/interface/ITributos';
+import supabase from '@/lib/supabase';
 
 export const inserirConfigTributo = async (info: ITributos) => {
   const { data, error } = await supabase
-    .from("db_tributo")
+    .from('db_tributo')
     .insert(info)
     .select();
 
-    if(error){
-        console.warn('Ocorreu um erro na gravação\n', error.message)
-        throw new Error('Ocorreu um erro na gravação dos dados')
-    }
+  if (error) {
+    console.warn('Ocorreu um erro na gravação\n', error.message);
+    throw new Error('Ocorreu um erro na gravação dos dados');
+  }
 
-    console.log(data);
-    
+  console.log(data);
+};
+
+export const recuperarListaTributoMensal = async () => {
+  let { data: db_tributo, error } = await supabase
+    .from('db_tributo')
+    .select('*');
+
+  if (error) {
+    console.warn(
+      'Ocorreu um erro ao tentar recuperar lista de Tributos\n',
+      error
+    );
+    throw new Error('Ocorreu um erro ao tentar recuperar lista de Tributos');
+  }
+
+  return db_tributo;
 };
