187269b5d0e8e3c9e87bf882f57adfb5246d6ed4
Jederson Andre <andre_sjx@live.com>
2024-06-03 15:56:18 -0300

    :recycle: refactor: modificação nas implementações das funções de calculo de inss retido e patraonal

--- src/componentes/formularioServico/calculos.ts
+++ src/componentes/formularioServico/calculos.ts
@@ -6,7 +6,23 @@ import { formatarCusto } from './action';
  * @param salario_base -string - salario  para calculo de retenção de INSS
  * @returns rs - string- com valor formatado para pt-br
  */
-export const calcularSegurado = (base: number, salario_base: number) => {
+export const calcularRetido = (base: number, salario_base: number, max_recolhimento:number) => {
+  console.log(salario_base)
+  if (
+    salario_base === 0 ||
+    salario_base === null ||
+    salario_base === undefined 
+  ) {
+    throw new Error('Preencha o campo CUSTOS');
+  }
+
+  const rs = (base / 100) * salario_base;
+
+  if(rs > max_recolhimento) return max_recolhimento
+  return parseFloat(rs.toFixed(2));
+};
+
+export const calcularPatronal = (base: number, salario_base: number)=>{
   if (
     salario_base === 0 ||
     salario_base === null ||
@@ -18,6 +34,9 @@ export const calcularSegurado = (base: number, salario_base: number) => {
   const rs = (base / 100) * salario_base;
 
   return parseFloat(rs.toFixed(2));
-};
+}
 
-export const CalcularPatronal = (base: number, salario_base: number) => {};
+export const totalImposto = (valor_retido:number, valor_patronal:number)=>{
+
+  return valor_retido+valor_patronal
+}
