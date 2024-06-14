f29dcb89aee3d9aa9ef252dbf784332e61167ad7
Jederson Andre <andre_sjx@live.com>
2024-06-14 11:58:16 -0300

    :recycle: refactor: modificação na logica, inclusao de provider, inicializando valores vazios no context local

--- src/app/configuracao/page.tsx
+++ src/app/configuracao/page.tsx
@@ -1,10 +1,11 @@
-'use client'
+"use client";
 import CadastroSecretariaDrawer from "@/componentes/drawers/CadastroSecretariasDrawer";
 import ConfiguracaoIncluirTeto from "@/componentes/drawers/ConfiguracaoIncluirTeto";
 import Head from "@/componentes/head/head";
 import BarraNavegacao from "@/componentes/navbar";
 import TabelaCompetencia from "@/componentes/tabela/tabelaCompetencia";
-import { Flex, HStack, Stack } from "@chakra-ui/react";
+import { TributoProvider } from "@/context/tributoContext";
+import { Flex, HStack, Stack,  } from "@chakra-ui/react";
 
 const Configuracao = () => {
   return (
@@ -13,12 +14,20 @@ const Configuracao = () => {
       <Flex flexDirection={"column"} height={"100vh"} width={"100vw"}>
         <Head text="Configuração" buttonReturn={true} />
         <HStack>
-          <ConfiguracaoIncluirTeto />
+          {/**
+           * Inclusao de provider neste ponto, pois ao chamar o componente de configuração
+           * não será puxado os valores que foram setados na tela home
+           * Este provider será outra intancia
+           */}
+          <TributoProvider>
+            <ConfiguracaoIncluirTeto />
+          </TributoProvider>
+
           <CadastroSecretariaDrawer />
         </HStack>
 
         <Stack padding={8}>
-            <TabelaCompetencia />
+          <TabelaCompetencia />
         </Stack>
       </Flex>
     </HStack>
