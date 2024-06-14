beedbfc06b1c4981d826995b026543213671a5c3
Jederson Andre <andre_sjx@live.com>
2024-06-14 11:56:56 -0300

    :recycle: refactor: add implementação para atualizaão de teto e configuração

--- src/componentes/tabela/tabelaCompetencia.tsx
+++ src/componentes/tabela/tabelaCompetencia.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { TributoContext } from "@/context/tributoContext";
 import {
@@ -12,15 +12,15 @@ import {
   TableContainer,
 } from "@chakra-ui/react";
 import { useContext } from "react";
+import AtualizarConfiguracaoDrawer from "../drawers/AtualizarConfiguracaoDrawer";
 
 const TabelaCompetencia = () => {
-  
-  const {tributoRef} = useContext(TributoContext)
+  const { tributoRef } = useContext(TributoContext);
 
   return (
     <>
       <TableContainer>
-        <Table variant="simple" size={'sm'}>
+        <Table variant="simple" size={"sm"}>
           <TableCaption>Imperial to metric conversion factors</TableCaption>
           <Thead>
             <Tr>
@@ -28,19 +28,18 @@ const TabelaCompetencia = () => {
               <Th>MAX_REC</Th>
               <Th>BASE % RETIDO</Th>
               <Th>BASE % PATRONAL</Th>
+              <Th>OPÇÕES</Th>
             </Tr>
           </Thead>
           <Tbody>
             <Tr>
               <Td>{tributoRef.competencia}</Td>
               <Td>{tributoRef.max_recolhimento}</Td>
-              <Td >{tributoRef.base_segurado}</Td>
-              <Td >{tributoRef.base_patronal}</Td>
-
+              <Td>{tributoRef.base_segurado}</Td>
+              <Td>{tributoRef.base_patronal}</Td>
+              <Td>{<AtualizarConfiguracaoDrawer />}</Td>
             </Tr>
-            
           </Tbody>
-         
         </Table>
       </TableContainer>
     </>
