9a7f787db3b98036e234686195c332f68e43507f
Jederson Andre <andre_sjx@live.com>
2024-06-28 15:51:48 -0300

    :sparkles: feat: implementação de service para buscar serviços lançados

--- src/service/servicosService.ts
+++ src/service/servicosService.ts
@@ -1,24 +1,45 @@
-import { IServico } from '@/interface/IServico';
-import { Freelance } from '@/interface/freelance';
-import supabase from '@/lib/supabase';
+import { IServico } from "@/interface/IServico";
+import { IPrestador } from "@/interface/IPrestador";
+import supabase from "@/lib/supabase";
 
 export const inserirNovoServico = async (
   servico: IServico,
-  autonomo: Freelance
+  autonomo: IPrestador
 ) => {
   const { data, error } = await supabase
-    .from('db_servicos')
+    .from("db_servicos")
     .insert({ ...servico, pispasep: autonomo.pisPasep })
     .select();
 
   if (error) {
     console.warn(
-      'Ocorreu um erro ao tentar registar novo serviço\n',
+      "Ocorreu um erro ao tentar registar novo serviço\n",
       error?.message
     );
-    throw new Error('Ocorreu um erro ao tentar registar novo serviço');
+    throw new Error("Ocorreu um erro ao tentar registar novo serviço");
   }
 
   console.log(data);
   return;
+};
+
+export const buscarServico = async (competencia: string) => {
+  
+  let { data: db_servico, error } = await supabase
+    .from("db_servico")
+    .select(
+      `*,
+      db_pessoas (pispasep, nome)`
+    )
+    .eq("competencia", competencia)
+
+
+  if (error) {
+    console.log("erro na consulta\n", error);
+    throw new Error(
+      "Ocorreu um erro na tentativa de recuperar serviços lançados"
+    );
+  }
+  console.log(db_servico);
+  return db_servico;
 };
